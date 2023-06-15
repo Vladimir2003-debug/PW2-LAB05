3f78e1ee479da948fff0c36d59d7997691bab752
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-06-14 22:57:02 -0500

    creado metodos para ver los post y listarlos

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from django.http import ( HttpResponse, 
+                          JsonResponse,
+                        )
+from .models import Post
 # Create your views here.
+
+def MyPersonalBlog(request):
+    blogs = Post.objects.all()
+    context = {
+        'blogs':blogs
+    }
+
+    return render(request, 'index.html',context)
+
+def ViewContent(request):
+    title = request.GET.get('name','')
+    post = get_object_or_404(Post, title = title )
+    print(post)
+    view = {
+        'text': post.text
+    }
+
+    return JsonResponse(view)
+
+def DeleteFile(request):
+
+    return JsonResponse()
