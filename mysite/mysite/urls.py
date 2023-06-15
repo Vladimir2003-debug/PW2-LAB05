b672d6210124ff8440fe2a60e8ba002a3d3c0278
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-06-14 22:57:27 -0500

    añadido url para listar y ver datos

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from blog.views import (
+                MyPersonalBlog,
+                ViewContent,
+                )
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',MyPersonalBlog, name = 'blog'),
+    path('view/',ViewContent, name = 'list'),
 ]
